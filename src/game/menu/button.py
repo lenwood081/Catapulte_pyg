203a6395d4f93de0626e1a0b63e34cd745a2d00b
lenwood <lenwood081@gmail.com>
2025-08-17 15:36:53 +1000

    working on a basic menu to improve testing and development of multiplayer communication

---
+++ src/game/menu/button.py
@@ -0,0 +1,20 @@
+import pygame
+
+"""
+A clickable object
+takes a image
+does some action
+"""
+class Button:
+    def __init__(self) -> None:
+        self.image = 
+        self.rect = 
+
+    def on_click(self):
+        # override
+        pass
+
+    def draw(self, surface):
+        pass
+
+        
